0c87886132916bf5155f44ff6a5b7b5560edf827
zoomjosue <jaigb@outlook.com>
2026-04-07 12:09:30 -0600

    Se agrego la comunicación

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,17 @@ const app = express();
 
 const sucursalRoutes = require('./routes/SucursalRoutes');
 const usuarioRoutes = require('./routes/UsuarioRoutes');
+const productosRoutes = require('./routes/ProductosRoutes');
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 app.get('/api/ping', (req, res) => {
   res.json({ status: 'ok', message: 'pong' });
@@ -13,6 +22,7 @@ app.get('/api/ping', (req, res) => {
 
 app.use('/api/sucursales', sucursalRoutes);
 app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/productos', productosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
